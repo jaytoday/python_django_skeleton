7171f182cff12c55abde50649cdfb131ac926605
Kristján Pétursson <kristjan@gmail.com>
2012-11-26 11:27:21 -0800

    Don't double-parse JSON
    The `Singly` module already parses the JSON. Doing it again in the view broke
    since it was already a Python object.

--- webapp/views.py
+++ webapp/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 
-try: import simplejson as json
-except ImportError: import json
-
-
 def index(request, template='index.html'):
     services = [
         'Facebook',
@@ -18,8 +14,7 @@ def index(request, template='index.html'):
     ]
     if request.user.is_authenticated():
         user_profile = request.user.get_profile()
-        # We replace single quotes with double quotes b/c of python's strict json requirements
-        profiles = json.loads(user_profile.profiles.replace("'", '"'))
+        profiles = user_profile.profiles
     response = render_to_response(
             template, locals(), context_instance=RequestContext(request)
         )
